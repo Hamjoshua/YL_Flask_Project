eeafaa0913f7a762fee044e4af52b3b77190f9c9
ddxdydz_console <Lizun57@inbox.ru>
2021-04-16 21:49:04 +0700

    HEROKU DEPLOYED

--- main.py
+++ main.py
@@ -515,11 +515,11 @@ def main():
     generate_routes()
     
     # Для heroku
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
     # Для локального тестирования
-    app.run(port=8000, host='127.0.0.1')
+    # app.run(port=8000, host='127.0.0.1')
 
 
 if __name__ == '__main__':
